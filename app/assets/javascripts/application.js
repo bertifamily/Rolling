9fc9d948a9bdefaa739b1d04cd8180fff168c7bd
Van Berti <bertifamily@optusnet.com.au>
2015-06-16 22:07:41 +0800

    First commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,33 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery-2.1.0
+//= require bootstrap.js
+//= require plugins/scrollto/jquery.scrollTo-1.4.3.1-min
+//= require plugins/scrollto/jquery.localscroll-1.2.7-min
+//= require plugins/easing/jquery.easing.min
+//= require plugins/google-map/google-map
+//= require plugins/parallax/jquery.parallax-1.1.3
+//= require plugins/jpreloader/jpreloader.min
+//= require plugins/isotope/imagesloaded.pkgd
+//= require plugins/isotope/isotope.pkgd.min
+//= require plugins/wow/wow
+//= require plugins/flexslider/jquery.flexslider-min
+//= require plugins/magnific/jquery.magnific-popup.min
+//= require plugins/supersized/supersized.3.2.7.min
+//= require plugins/supersized/supersized.shutter.min
+//= require loop
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require bootstrap-sprockets
+//= require_tree .
